38626b81b3785ffc0729c7cfb6f003dbbd6a4230
Дмитрий Чамкин <dima.chamkin20221@outlook.com>
2024-01-14 20:12:33 +0300

    Добавьте файлы проекта.

--- Naming.h
+++ Naming.h
@@ -3,8 +3,12 @@
 #ifndef naming_header
 #define naming_header
 
-#define program_name L"Algebradabra"
-#define program_version L"1.0.2a"
+#define program_version_abovepublic L"v1.0.3"
+#define program_version_publication L"1build"
+#define program_version_family L"May"
+#define program_version program_version_family L" " program_version_publication L" " program_version_abovepublic
+
+#define program_name L"Algebradabra " program_version_family
 #define program_author L"Д. М. Чамкин"
 
 #define main_window_class_name L"Main window"
