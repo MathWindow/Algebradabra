d830362fb1bee0e966ee390b0a0f8393c1a14505
Дмитрий Чамкин <dima.chamkin20221@outlook.com>
2024-01-14 20:12:33 +0300

    Добавьте файлы проекта.

--- VersionInfo.h
+++ VersionInfo.h
@@ -10,21 +10,9 @@
 #if turn_macro_off == 0
 
 #define program_version_abovepublic L"v1.1"
-#define program_version_publication L"23build"
+#define program_version_publication L"24build"
 #define program_version_family L"May"
 #define program_version program_version_family L" " program_version_publication L" " program_version_abovepublic
-
-#define program_name L"Algebradabra " program_version_family
-#define program_author L"Ä. Ì. ×àìêèí"
-
-#define main_window_class_name L"Main window"
-#define main_window_name program_name L" — main window"
-
-#define error_list_window_class_name L"Error list window"
-#define error_list_window_name program_name L" — error list"
-
-#define algebraic_book_create_window_class_name L"Algebraic book create window"
-#define algebraic_book_create_name program_name L" — create algebraic book"
 
 #else
 LPCWSTR program_name = L"Algebradabra";
